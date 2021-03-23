c4228cf8f94fc04d362457e49f11f129eab65a29
aidxn <asmaidan@outlook.com>
2021-03-23 16:57:32 -0500

    Init

---
+++ rocket-app/target/debug/deps/rocket_app.d
@@ -0,0 +1,5 @@
+/Users/aidanm/Documents/GitHub/rust-backend/rocket-app/target/debug/deps/rocket_app: src/main.rs
+
+/Users/aidanm/Documents/GitHub/rust-backend/rocket-app/target/debug/deps/rocket_app.d: src/main.rs
+
+src/main.rs:
