cffd427048d546d97f679d66d2509cc165546a21
EDUARDO FLORES HINOJOZA <floresh.eduardo@sonora.edu.mx>
2021-03-24 10:44:11 -0700

    gitignore

--- rocket-app/target/debug/deps/rocket_app.d
+++
@@ -1,5 +0,0 @@
-/Users/aidanm/Documents/GitHub/rust-backend/rocket-app/target/debug/deps/rocket_app: src/main.rs
-
-/Users/aidanm/Documents/GitHub/rust-backend/rocket-app/target/debug/deps/rocket_app.d: src/main.rs
-
-src/main.rs:
